6a2ec4fa5da1123e03c234d7e9013d99d9312b05
Jose.Hernandez <joey.hernandezjr@gmail.com>
2013-08-01 16:55:06 -0400

    Wacky Commit

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,4 +1,22 @@
 // JavaScript
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+var myvariable
+myvariable = "This is my Wacky World";
+alert(myvariable);
+
+
+var children = prompt("How many children do you have")
+var friends = prompt("How many friends do you have")
+var family = prompt("How many family members do you have")
+
+
+var total = (children + friends + family)
+alert(total)
+
+if (total < 10){
+	document.write("Please find some more friends")
+};
+alert("Great you know a lot of people, I will send you an invitation to our grand opening of our new clothing store")
+if (total > 10){
+	document.write("Please bring this invitation for an extra 75% off everything in the store")
+}
