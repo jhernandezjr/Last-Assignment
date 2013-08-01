dd80a063faaf593ae0017c1e89039e412dabe4aa
Jose.Hernandez <joey.hernandezjr@gmail.com>
2013-08-01 15:43:16 -0400

    Personal Commit

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,30 @@
 // JavaScript
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+
+var myvariable
+myvariable = "You must answer these questions before entering the club";
+alert(myvariable);
+
+
+var age = prompt("What is your age")
+
+if (age <17){
+	document.write("You are not of legal age to enter the club")
+};
+
+if (age <21){
+	document.write("You are not of the legal age to drink at the club")
+};
+
+if (age >18){
+	document.write("Welcome, you may enter our club")
+	}
+
+var question = prompt("Do you have a valid ID");
+
+
+
+var question = prompt("Do you have a job");
+
+
+
