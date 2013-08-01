e73af865077a37f22446b7e9cf89bac9b0c09686
Jose.Hernandez <joey.hernandezjr@gmail.com>
2013-08-01 16:12:20 -0400

    Industrial Commit

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,4 +1,20 @@
 // JavaScript
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+var myvariable
+myvariable = "If you are human click here";
+alert(myvariable);
+
+var employees = prompt("How many employees do you have")
+
+if (employees < 10){alert("You are required to have min of 10 employees")
+}
+var budget = prompt("What is your budget that you want to spend on new computers")
+
+if (budget < 5000){document.write("You will need more money so you can upgrade your old computers")
+}
+
+
+if (budget > 5000){document.write("Great lets order your computers today")
+}
+
+
